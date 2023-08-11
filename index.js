92d14c85abca5f49fbcd63c68bfbc24b320d49b4
Duke <dukemorise33@gmail.com>
2023-08-11 14:21:14 -0700

    completed find lab phase-1

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+
+
+function superbowlWin(record) {
+
+    let filteredRecord = record.find((item) => item.result === 'W');
+
+    if (filteredRecord) {
+        return filteredRecord.year;
+    }
+    else {
+        return undefined;
+    }
+
+}
+
+
+
